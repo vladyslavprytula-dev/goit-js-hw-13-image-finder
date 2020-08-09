f4ffba856eabc7e7ab4274177efbd672fea9043e
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-08-09 21:30:36 +0300

    updated interface

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { searchFormSubmit, loadMore } from './js/functionForListeners';
+import { searchFormSubmit, loadMore } from './js/functionForListeners.js';
 const searchForm = document.querySelector('.js-search');
 const loadMoreBtn = document.querySelector('.js-btn');
 
